4cfd533f23c9a009be5e89fa60eab3776db73266
jhomer Vilcamiche <vbjhomer@gmail.com>
2019-05-25 12:33:29 -0500

    segundo commit

--- js/masonry.pkgd.js
+++ js/masonry.pkgd.js
@@ -1073,7 +1073,7 @@ proto.transition = function( args ) {
     this.css( args.from );
     // force redraw. http://blog.alexmaccaw.com/css-transitions
     var h = this.element.offsetHeight;
-    // hack for JSHint to hush about unused var
+    // hack for JSHint to hush nosotros unused var
     h = null;
   }
   // enable transition
